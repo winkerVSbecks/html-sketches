f7c7d0916534e9612f1e34553bcaadf12b64a464
winkerVSbecks <varunvachhar@gmail.com>
2023-12-06 21:50:13 -0500

    displacements

--- sketches/displacements/index.ts
+++ sketches/displacements/index.ts
@@ -4,10 +4,66 @@ import Random from 'canvas-sketch-util/random';
 import { gsap } from 'gsap';
 import { Flip } from 'gsap/Flip';
 import { colorScheme } from '../../src/colors/full-pipe';
+import chroma from 'chroma-js';
 
 gsap.registerPlugin(Flip);
 
-const { background, foreground } = colorScheme(6, true);
+const { colors } = colorScheme(6, true);
+
+const background = colors.pop()!;
+const foreground = colors.filter((c: string) => {
+  return chroma.contrast(background, c) > 1.25;
+});
+
+const noise = (color: string) => `
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  version="1.1"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+  xmlns:svgjs="http://svgjs.dev/svgjs"
+  width="100%"
+  height="100%"
+>
+  <defs>
+    <filter
+      id="nnnoise-filter"
+      x="-20%"
+      y="-20%"
+      width="140%"
+      height="140%"
+      filterUnits="objectBoundingBox"
+      primitiveUnits="userSpaceOnUse"
+      color-interpolation-filters="linearRGB"
+    >
+      <feTurbulence
+        type="fractalNoise"
+        baseFrequency="0.102"
+        numOctaves="4"
+        seed="15"
+        stitchTiles="stitch"
+        x="0%"
+        y="0%"
+        width="100%"
+        height="100%"
+        result="turbulence"
+      ></feTurbulence>
+      <feSpecularLighting
+        surfaceScale="15"
+        specularConstant="0.75"
+        specularExponent="20"
+        lighting-color="${color}"
+        x="0%"
+        y="0%"
+        width="100%"
+        height="100%"
+        in="turbulence"
+        result="specularLighting"
+      >
+        <feDistantLight azimuth="3" elevation="100"></feDistantLight>
+      </feSpecularLighting>
+    </filter>
+  </defs>
+</svg>`;
 
 const randomColor = () => Random.pick(foreground);
 
@@ -24,16 +80,19 @@ const gridStyles = (
   'grid-gap': gap,
   'grid-auto-flow': 'dense',
   height: '100%',
+  position: 'relative',
 });
 
-const gridItem = (x: number, y: number) => {
+const gridItem = (w: number, h: number, x?: number, y?: number) => {
   const el = document.createElement('div');
   el.classList.add('item');
   el.setAttribute('data-flip-key', Math.random().toString());
 
   css(el, {
-    // @ts-ignore-next-line
-    'grid-area': `auto / auto / span ${y} / span ${x}`,
+    'grid-area':
+      x !== undefined && y !== undefined
+        ? `${y} / ${x} / span ${h} / span ${w}`
+        : `auto / auto / span ${h} / span ${w}`,
     'background-color': randomColor(),
   });
 
@@ -41,10 +100,26 @@ const gridItem = (x: number, y: number) => {
 };
 
 const generateItems = (count: number, columns: number, rows: number) => {
-  return Array.from({ length: count }).map(() => ({
-    x: Random.rangeFloor(2, Math.floor(columns * 0.75)),
-    y: Random.rangeFloor(2, Math.floor(rows * 0.75)),
-  }));
+  return Array.from({ length: count }).map(() => {
+    const overlap = Random.chance(0.75);
+
+    if (overlap) {
+      const x = Random.rangeFloor(0, columns - 2);
+      const y = Random.rangeFloor(0, rows - 2);
+
+      return {
+        x,
+        y,
+        w: Random.rangeFloor(2, columns - x),
+        h: Random.rangeFloor(2, rows - y),
+      };
+    }
+
+    return {
+      w: Random.rangeFloor(2, columns),
+      h: Random.rangeFloor(2, rows),
+    };
+  });
 };
 
 function sketch() {
@@ -56,9 +131,9 @@ function sketch() {
 
   const root = document.querySelector<HTMLDivElement>('#app')!;
   css(root, {
-    height: `calc(100dvh - ${gap} * 2)`,
-    padding: gap,
-    background: background,
+    height: `calc(100dvh - ${gap} * 8)`,
+    padding: `calc(4 * ${gap})`,
+    // background: background,
   });
 
   const grid = document.createElement('div');
@@ -69,18 +144,41 @@ function sketch() {
   const itemEls: HTMLElement[] = [];
 
   items.forEach((item) => {
-    const itemEl = gridItem(item.x, item.y);
+    const itemEl = gridItem(item.w, item.h, item.x, item.y);
     grid.appendChild(itemEl);
     itemEls.push(itemEl);
   });
+
+  const noiseEl = document.createElement('div');
+  noiseEl.innerHTML = noise(background);
+  css(noiseEl, {
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    width: '100%',
+    height: '100%',
+    filter: "url('#nnnoise-filter')",
+    'background-blend-mode': 'hard-light',
+  });
+  grid.appendChild(noiseEl);
 
   window.setInterval(() => {
     const state = Flip.getState(itemEls);
 
     const newItems = generateItems(count, columns, rows);
     itemEls.forEach((el, idx) => {
-      const { x, y } = newItems[idx];
-      el.style.setProperty('grid-area', `auto / auto / span ${y} / span ${x}`);
+      const { x, y, w, h } = newItems[idx];
+      if (x !== undefined && y !== undefined) {
+        el.style.setProperty(
+          'grid-area',
+          `${y} / ${x} / span ${h} / span ${w}`
+        );
+      } else {
+        el.style.setProperty(
+          'grid-area',
+          `auto / auto / span ${y} / span ${x}`
+        );
+      }
     });
 
     Flip.from(state, {
@@ -96,7 +194,7 @@ window.onload = () => {
   sketch();
 };
 
-function css(element: HTMLElement, style: Partial<CSSStyleDeclaration>) {
+function css(element: HTMLElement, style: any) {
   for (const property in style) {
     element.style[property as string] = style[property];
   }
--- src/colors/full-pipe.ts
+++ src/colors/full-pipe.ts
@@ -2,10 +2,6 @@ import { Hsluv } from 'hsluv';
 import Random from 'canvas-sketch-util/random';
 import chroma from 'chroma-js';
 
-// Based on https://programmingdesignsystems.com/color/color-schemes/index.html
-
-// oklch( [ lightness = <percentage (0%-100%)> ] [ chroma <number> (0-0.37) ] [ hue <degrees> (0deg-360deg) ]
-
 export function lightnessToLuminance(l: number) {
   if (l <= 8) {
     return (1.0 * l) / 903.2962962;
@@ -19,61 +15,6 @@ export function contrastRatio(l1: number, l2: number) {
   l2 = lightnessToLuminance(l2);
   return (l1 + 0.05) / (l2 + 0.05);
 }
-
-// interface HSLPart {
-//   range: [number, number];
-//   easing: (x: number) => number;
-// }
-
-// interface Hue {
-//   start: number;
-//   cycles: number;
-//   easing: (x: number) => number;
-// }
-
-// interface GenerateHSLRampOptions {
-//   total: number;
-//   hue: Hue;
-//   saturation: HSLPart;
-//   lightness: HSLPart;
-// }
-
-// export function generateHSLRamp({
-//   total = 9,
-//   hue: h,
-//   saturation: s,
-//   lightness: l,
-// }: GenerateHSLRampOptions) {
-//   const lDelta: number = l.range[1] - l.range[0];
-//   const sDelta: number = s.range[1] - s.range[0];
-
-//   return new Array(total).fill(0).map((_, idx) => {
-//     const relI = idx / (total - 1);
-
-//     return [
-//       (360 + h.start + (1 - h.easing(relI)) * (360 * h.cycles)) % 360,
-//       s.range[0] + sDelta * s.easing(relI),
-//       l.range[0] + lDelta * l.easing(relI),
-//     ];
-//   });
-// }
-
-/*  let hsluvColors = generateHSLRamp({
-    total: count,
-    hue: {
-      start: Random.rangeFloor(0, 360),
-      easing: (x: number) => Math.pow(x, 2),
-      cycles: 1,
-    },
-    saturation: {
-      range: [40, 35],
-      easing: (x: number) => Math.pow(x, 2),
-    },
-    lightness: {
-      range: [Random.range(10, 40), 90],
-      easing: (x: number) => Math.pow(x, 1.5),
-    },
-  }) */
 
 interface HSLPart {
   range: [number, number];
@@ -100,11 +41,8 @@ export function generateHSLRamp({
     const relI = idx / (total - 1);
 
     return [
-      // h.range[0] + idx * h.range[1],
       (360 + h.range[0] + (1 - h.easing(relI)) * (360 * h.cycles)) % 360,
       s.range[0] + sDelta * s.easing(relI),
-      // s.range[0], // + i * range[1],
-      // l.range[0] + relI * l.range[1],
       l.range[0] + lDelta * l.easing(relI),
     ];
   });
@@ -142,7 +80,6 @@ export function colorScheme(count: number = 3, debug = false) {
   const background = Random.pick(colors);
   // Filter out colors that don't have enough contrast with the background
   const foreground = colors.filter((c: string) => {
-    if (c === background) return false;
     return chroma.contrast(background, c) > 1.25;
   });
 
